c1cb42c10af1b958fec3d13bb4bfe9153e8f3b5d
fernando.farias <fernandofarias527@gmail.com>
2024-10-28 20:50:54 -0300

    run metrics

--- app/Services/GooglePageSpeedService.php
+++ app/Services/GooglePageSpeedService.php
@@ -12,23 +12,35 @@ class GooglePageSpeedService
     public function __construct()
     {
         $this->client = new Client([
-            'timeout' => 60,  
+            'timeout' => 60,
         ]);
         $this->apiKey = env('GOOGLE_API_KEY');
     }
 
     public function getMetrics($url, $categories, $strategy)
     {
+        // Preparar la URL para la consulta
         $categoriesParam = implode('&category=', $categories);
         $apiUrl = "https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url={$url}&key={$this->apiKey}&category={$categoriesParam}&strategy={$strategy}";
-
         try {
-            $response = $this->client->request('GET', $apiUrl);
+            // Realizar la solicitud
+            $response = $this->client->get($apiUrl);
             $data = json_decode($response->getBody()->getContents(), true);
 
-            return $data;
+            // Extraer las métricas necesarias de `lighthouseResult.categories`
+            $metrics = [];
+            if (isset($data['lighthouseResult']['categories'])) {
+                foreach ($data['lighthouseResult']['categories'] as $key => $category) {
+                        // Guardamos el nombre y el puntaje de cada métrica relevante
+                        $metrics[$category['title']] = $category['score'] * 100; // Convertimos el score a porcentaje
+                }
+            }
+
+            return $metrics;
         } catch (\Exception $e) {
+            \Log::error('error fetching metrics'. $e->getMessage());
             throw new \Exception('Error fetching metrics from Google API');
         }
     }
+
 }
